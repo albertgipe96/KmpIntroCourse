daffc13a9f938c45d1b651285d92bddc5973bab2
albertgipe96 <albertgipe96@gmail.com>
2024-05-05 16:19:46 +0200

    Add animation to switch button

--- composeApp/src/commonMain/kotlin/ui/presentation/components/header/CurrencyInputs.kt
+++ composeApp/src/commonMain/kotlin/ui/presentation/components/header/CurrencyInputs.kt
@@ -1,5 +1,7 @@
 package ui.presentation.components.header
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +11,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import domain.model.Currency
 import domain.model.RequestState
@@ -27,6 +34,12 @@ fun CurrencyInputsRow(
     targetCurrency: RequestState<Currency>,
     onSwitchClick: () -> Unit
 ) {
+    var animationStarted by remember { mutableStateOf(false) }
+    val animatedRotation by animateFloatAsState(
+        targetValue = if (animationStarted) 180F else 0F,
+        animationSpec = tween(500)
+    )
+
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
@@ -38,8 +51,15 @@ fun CurrencyInputsRow(
         )
         Spacer(modifier = Modifier.width(16.dp))
         IconButton(
-            modifier = Modifier.padding(top = 24.dp),
-            onClick = onSwitchClick
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .graphicsLayer {
+                    rotationY = animatedRotation
+                },
+            onClick = {
+                animationStarted = !animationStarted
+                onSwitchClick()
+            }
         ) {
             Icon(
                 modifier = Modifier.size(24.dp),
