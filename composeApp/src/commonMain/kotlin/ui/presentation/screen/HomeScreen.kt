ce0a4eecbcfd28f1563a1a60ef9cb87653a2cf20
albertgipe96 <albertgipe96@gmail.com>
2024-05-11 21:08:42 +0200

    Currency Picker Dialog

--- composeApp/src/commonMain/kotlin/ui/presentation/screen/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/presentation/screen/HomeScreen.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
+import domain.model.CurrencyType
+import ui.presentation.components.dialog.CurrencyPickerDialog
 import ui.presentation.components.header.HomeHeader
 import ui.theme.surfaceColor
 
@@ -21,8 +24,12 @@ class HomeScreen : Screen {
         val rateStatus by viewModel.rateStatus
         val sourceCurrency by viewModel.sourceCurrency
         val targetCurrency by viewModel.targetCurrency
+        val allCurrencies = remember { mutableStateOf(viewModel.allCurrencies) }
 
         var amount by rememberSaveable { mutableStateOf(0.0) }
+
+        var selectedCurrencyType by remember { mutableStateOf(CurrencyType.None) }
+        var dialogOpened by remember { mutableStateOf(true) }
 
         Column(
             modifier = Modifier
@@ -37,6 +44,15 @@ class HomeScreen : Screen {
                 onRefresh = { viewModel.sendEvent(HomeUiEvent.RefreshRates) },
                 onSwitchClick = { viewModel.sendEvent(HomeUiEvent.SwitchCurrencies) },
                 onAmountValueChange = {  }
+            )
+        }
+
+        if (dialogOpened) {
+            CurrencyPickerDialog(
+                currencyList = allCurrencies.value,
+                currencyType = selectedCurrencyType,
+                onConfirmClick = { dialogOpened = false },
+                onDismiss = { dialogOpened = false }
             )
         }
     }
